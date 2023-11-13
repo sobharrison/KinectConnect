f8de1b2b227425ed48d969b5003fe2aab20518f9
Jshadbolt001 <70242314+Jshadbolt001@users.noreply.github.com>
2023-11-12 23:34:11 -0600

    Added Body Tracking to the Milestone 3 demo

--- MileStone3/HandTracker.py
+++ MileStone3/HandTracker.py
@@ -2,42 +2,54 @@ import mediapipe as mp
 import cv2
 import numpy as np
 
+# Initialize MediaPipe Pose, Hands, and Drawing Modules
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
+mp_pose = mp.solutions.pose
 
+#Initializing the Deteectors
+hand_detector = mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence = 0.5)
+pose_detector = mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5)
+
+#OpenCV VideoCapture
 cap = cv2.VideoCapture(0)
 
-with mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence = 0.5) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
 
-        #changing the frames color to coincide with the mediapipe library and flipping the image
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image = cv2.flip(image, 1)
+while cap.isOpened():
 
-        #Processing the image to detect the hands, and toggling flags as need be
-        image.flags.writeable = False
-        results = hands.process(image)
-        image.flags.writeable = True
+    #Read a frame from the webcam
+    return_value, image = cap.read()
+    if not return_value:
+        break
 
-        #changing the color back to display later
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        #print(results) #this is just some optional debug info
+    #flipping the webcam input
+    image = cv2.flip(image, 1)
 
-        #this renders the "skeleton" of the hands. fits the colorscheme from our "paper prototype"
-        if results.multi_hand_landmarks:
-            for num, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
-                                          mp_drawing.DrawingSpec(color=(246, 100, 20), thickness=2, circle_radius=4),
-                                          mp_drawing.DrawingSpec(color=(248, 200, 0), thickness=2, circle_radius=2))
+    #Convert the BGR image to RGB
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        #finally displaying the image
-        cv2.imshow('Hand Tracking', image)
 
-        #This closes the program if the user hits the 'q' key on their keyboard
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+    #Detecting Hands and Poses
+    hand_results = hand_detector.process(image_rgb)
+    pose_results = pose_detector.process(image_rgb)
 
-#cleanup after the program is done
+    #Rendering landmarks on the image
+    joint_styling = mp_drawing.DrawingSpec(color = (246, 100, 20), thickness = 2, circle_radius = 4)
+    line_styling = mp_drawing.DrawingSpec(color = (248, 200, 0), thickness = 2, circle_radius = 2)
+    if hand_results.multi_hand_landmarks:
+        for landmarks in hand_results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(image, landmarks, mp_hands.HAND_CONNECTIONS, joint_styling, line_styling)
+    if pose_results.pose_landmarks:
+        mp_drawing.draw_landmarks(image, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS, joint_styling, line_styling)
+
+
+    #finally displaying the image
+    cv2.imshow('Hand Tracking', image)
+
+    #This closes the program if the user hits the 'q' key on their keyboard
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+#Cleanup after the program is done
 cap.release()
 cv2.destroyAllWindows()
--- MileStone3/ReadMe.md
+++ MileStone3/ReadMe.md
@@ -1,7 +1,4 @@
 # Milestone 3 Demo
 
-This is a demonstration of hand trackign working with the mediapipe library, able to track the position of the users hands and all the joints that make them up.
-
-This will be used heavily to estimate poses that will be cconverted into input on the users machine.
-
-[Code is adapted from](https://www.youtube.com/watch?v=06TE_U21FK4)
+This is a demonstration of hand and pose(body) tracking working with the mediapipe library.
+This will be used heavily to estimate poses that will be converted into input on the users machine.
